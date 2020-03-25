bfbbfe64b076c25556d385e1862bfafb596a2839
“TiffanyCasey” <tiffanylynnecasey@gmail.com>
2020-03-24 22:50:10 -0400

    WIP fixed initial form and updated question variables

--- script.js
+++ script.js
@@ -4,34 +4,75 @@ var start = document.getElementById("start");
 var submitButton = document.getElementById("submit");
 var explanation = document.getElementById("explanation");
 var submitButton = document.getElementById("submitButton");
-var quizQuestionHeader = document.getElementById("quizQuestions");
+var quizQuestionHeader = document.getElementById("quizQuestionHeader");
 var choice1 = document.getElementById("1");
 var choice2 = document.getElementById("2");
 var choice3 = document.getElementById("3");
 var choice4 = document.getElementById("4");
+var correct = document.getElementById("correct");
 var answerResponse = document.getElementById("answerResponse");
 var allDone = document.getElementById("allDone")
 var finalScoreIs = document.getElementById("finalScoreIs");
 var clearHighScores = document.getElementById("clearHighScores");
+var questionButton = document.getElementById("questionBotton");
+
+var quizQuestionsPage = document.getElementById("quizQuestionsPage");
 
 
 //Variable Classes 
 var quizChallengePage = document.querySelector(".quizChallengePage");
-var quizQuestionsPage = document.querySelector(".quizQuestionsPage");
 var finalScorePage = document.querySelector(".finalScorePage");
+
+  // Quiz Questions
+  var quizQuestions = [
+    [quizQuestionHeader, "Commonly used Data Types Include:"], 
+    [1, "1. strings"],
+    [2, "2. booleans"],
+    [3, "3. alerts"],
+    [4, "4. numbers"],
+    [correct, ""],
+,
+    [quizQuestionHeader, "The condition in an if / else statement is enclosed within ________."],
+    [1, "1. quotes"],
+    [2, "2. curly brackets"],
+    [3, "3. parenthesis"],
+    [4, "4. square brackets"],
+    [correct, "3. parenthesis"],
+,
+    [quizQuestionHeader, "Arrays in JavaScript can be used to store ________."],
+    [1, "1. numbers and strings"],
+    [2, "2. other arrays"],
+    [3, "3. booleans"],
+    [4, "4. all of the above"],
+    [correct, "4. all of the above"],
+,
+    [quizQuestionHeader, "String values must be enclosed within ________ when being assigned to variables"],
+    [1, "1. commas"],
+    [2, "2. curly brackets"],
+    [3, "3. quotes"],
+    [4, "4. parenthesis"],
+    [correct, "3. quotes"],
+,
+    [quizQuestionHeader, "A very useful tool used for developing and debugging for printing content to the debugger is:"],
+    [1, "1. JavaScript"],
+    [2, "2. terminal / bash"],
+    [3, "3. for loops"],
+    [4, "4. console.log"],
+    [correct, "4. console.log"],
+]
 
 //Coding Quiz Challenge Page 
 start.textContent = "Coding Quiz Challenge";
 explanation.textContent = "Try to answer the following code-related questions within 80 seconds. Every incorrect answers will penalize your time by ten seconds";
 submitButton.textContent = "Start Quiz";
 
-quizQuestionsPage.style.display = "none";// Hide Quiz Questions Page
+//quizQuestionsPage.style.display = "none";// Hide Quiz Questions Page
 finalScorePage.style.display = "none"; // Hide Final Core Page 
 
 // START QUIZ / TIMER FUNCTION
-submitButton.addEventListener("click", startQuiz); // Start Quiz Button 
+submitButton.addEventListener("click", startQuiz)  // Start Timer Button 
 
-  var secondsLeft = 80; // Seconds in Timer 
+  var secondsLeft = 10; // Seconds in Timer 
   var startScore = 0; // Starting time 
   var finalScore = "";  // Holder for Final Time 
   var timer = document.getElementById("timer"); // Timer Variable 
@@ -40,7 +81,7 @@ timer.textContent = "Time: " + startScore; // Holder text in nav bar
 
 function startQuiz() { // Timer function Begins 
   finalScorePage.style.display = "none"; // Hide Final Core Page 
-  quizChallengePage.style.display ="none"; // Hide Quiz Challenge Page 
+  quizChallengePage.style.display = "none"; // Hide Quiz Challenge Page 
   quizQuestionsPage.style.display = "block"; // Show Quiz Questions Page
 
   var timerInterval = setInterval(function() {
@@ -52,39 +93,18 @@ function startQuiz() { // Timer function Begins
     }
   }, 1000);
 
-// Question Function - NEED TO FIX
-
-function askQuestions() {
-  questionNumber++;
-  answer = questions[questionNumber].answer
-
-  questionHead.textContent = questions[questionNumber].title;
-  answerChoices.innerHTML = "";
-
-  var choices = questions[questionNumber].choices;
-
-// Loop Questions - NEED TO FIX 
-  for (var i = 0; i < choices.length; i++) {
-      var answerResponse = document.createElement("button");
-
-      answerResponse.textContent = choices[i]
-      answerBtn = answerChoices.appendChild(answerResponse).setAttribute("class", "button");
-
- 
-  
-    answerResponse.innerHTML = "";
-
+  // Loop Questions - NEED TO FIX 
+  for (var i = 0; i < quizQuestions.length; i++) {
     
-  
-      if (answerResponse == quizQuestions[i].correct) {
+      if (questionButton === quizQuestions[5]) {
         startScore++;
         answerResponse.textContent = "Correct!";
       } else {
         answerResponse.textContent = "Wrong!";
+        startScore - 10; 
       }
     }
   }
-}
 
 // Go to All Done page & Print Score
 function finalScorePage() {
@@ -92,26 +112,28 @@ function finalScorePage() {
   quizQuestionsPage.style.display = "none"; // Hide Questions Page
   finalScorePage.style.display = "block"; // Show Final Score Page 
 
+  if (secondsLeft === 0 || quizQuestions.length === null) {
     allDone.textContent = "All done!";
     finalScoreIs.textContent = "Your final score is " + finalScore;
   }
+}
 
 //Function to add in initials - THINK IS DONE 
 var initials = document.getElementById("initials");
-var intitialInput = document.getElementById("initialInput")
+var intitialInput = document.getElementById("initialInput");
 var initialButton = document.getElementById("initialButton");
+
 var submittedInitial = document.getElementById("response");
 
 initialButton.textContent = "Submit";
-initials.textContent = "Enter Initials";
+initials.textContent = "Enter Your Initials: ";
 
+//submitButton.addEventListener("click", startQuiz)  // Start Timer Button 
 initialButton.addEventListener("click", function(event) { // click takes you to the high score page 
   event.preventDefault();
-  
-  console.log(event);
-   
+     
    var response = intitialInput.value;
-    submittedInitial.textContent = response; 
+   submittedInitial.textContent = response; 
  });
 
  // Save score (use.value?)
@@ -127,49 +149,7 @@ function showScore() {
 }
 
  
- // Questions
-var quizQuestions = [
-  {
-      question: "Commonly used Data Types Include:", 
-      choice1: "1. strings",
-      choice2: "2. booleans",
-      choice3: "3. alerts",
-      choice4: "4. numbers",
-      correct: "3",
-  },
-  {
-      question: "The condition in an if / else statement is enclosed within ________.",
-      choice1: "1. quotes",
-      choice2: "2. curly brackets",
-      choice3: "3. parenthesis",
-      choice4: "4. square brackets",
-      correct: "3",
-  },
-  {
-      question: "Arrays in JavaScript can be used to store ________.",
-      choice1: "1. numbers and strings",
-      choice2: "2. other arrays",
-      choice3: "3. booleans",
-      choice4: "4. all of the above",
-      correct: "4",
-  },
-  {
-      question: "String values must be enclosed within ________ when being assigned to variables",
-      choice1: "1. commas",
-      choice2: "2. curly brackets",
-      choice3: "3. quotes",
-      choice4: "4. parenthesis",
-      correct: "3",
-  },
-  {
-      question: "A very useful tool used for developing and debugging for printing content to the debugger is:",
-      choice1: "1. JavaScript",
-      choice2: "2. terminal / bash",
-      choice3: "3. for loops",
-      choice4: "4. console.log",
-      correct: "4",
-  },
-];
+
 
 
 
